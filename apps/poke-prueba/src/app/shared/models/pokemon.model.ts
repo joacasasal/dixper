9e0b18eec5df45db3e9bc374a1f9c080df2c1678
jcasasalbertos <joaquin.casasalbertos@altran.com>
2020-09-16 16:15:38 +0200

    Actualizado modelo Pokemon con PokeApi.

--- apps/poke-prueba/src/app/shared/models/pokemon.model.ts
+++ apps/poke-prueba/src/app/shared/models/pokemon.model.ts
@@ -1,12 +1,32 @@
 export class Pokemon {
-    id: string;
-    states?: any[];
+    id?: number;
+    name: string;
+    url?: string;
+    sprites?: any;
+    stats?: any[];
+    types?: any[];
+    abilities?: any[];
+    base_experience?: number;
+    forms?: any[];
+    game_indices?: any[];
+    height?: number;
+    held_items?: any[];
+    is_default?: boolean;
+    location_area_encounters?: string;
+    moves?: any[];
+    order?: number;
+    species?: any;
+    weight?: number;
 
     selected?: boolean;
     
     constructor(data: any) {
         this.id = data.id;
-        this.states = data.states ? data.states : [];
+        this.name = data.name;
+        this.url = data.url;
+        this.sprites = data.sprites ? data.sprites : [];
+        this.stats = data.stats ? data.stats : [];
+        this.types = data.types ? data.types : [];
 
         this.selected = data.selected;
     }
