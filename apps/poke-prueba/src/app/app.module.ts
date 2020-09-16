e4d6532d31822911f1e7e77d429c5d34b0dfe624
jcasasalbertos <joaquin.casasalbertos@altran.com>
2020-09-16 09:11:16 +0200

    Añadido BrowserAnimations al module.

--- apps/poke-prueba/src/app/app.module.ts
+++ apps/poke-prueba/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ const services = [
   declarations: [AppComponent, ...components],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule
   ],
