7f796072310d742091ba57d5633eb67380891a1a
jcasasalbertos <joaquin.casasalbertos@altran.com>
2020-09-17 12:58:42 +0200

    Añadida animation al detailSidebar.

--- apps/poke-prueba/src/app/modules/home/home.page.ts
+++ apps/poke-prueba/src/app/modules/home/home.page.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Store } from '@ngrx/store';
-import { AppState } from '../../app.module';
-
-import { Pokemon } from '../../shared/models/pokemon.model';
-
 /**
  * Vista principal.
  */
@@ -13,21 +8,8 @@ import { Pokemon } from '../../shared/models/pokemon.model';
 })
 export class HomePage implements OnInit {
 
-  public showDetails = false;
-
   constructor(
-    private store: Store<AppState>
-  ) {
-    this.store.select(state => state.pokemones).subscribe((pokemones) => {
-      if (pokemones && pokemones.list && pokemones.list.length > 0) {
-        if (pokemones.list.find((pokemon: Pokemon) => pokemon.selected)) {
-          this.showDetails = true;
-        } else {
-          this.showDetails = false;
-        }
-      }
-   });
-  }
+  ) { }
 
   ngOnInit() {
   }
--- apps/poke-prueba/src/app/shared/components/poke-details-sidebar/poke-details-sidebar.component.ts
+++ apps/poke-prueba/src/app/shared/components/poke-details-sidebar/poke-details-sidebar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
+
+import { Store } from '@ngrx/store';
+import { AppState } from '../../../app.module';
+
+import { Pokemon } from '../../models/pokemon.model';
 
 /**
  * Componente que muestra un Sidebar con los Detalles de un Pokémon.
@@ -6,13 +12,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'dixper-poke-details-sidebar',
   templateUrl: './poke-details-sidebar.component.html',
-  styleUrls: ['./poke-details-sidebar.component.scss']
+  styleUrls: ['./poke-details-sidebar.component.scss'],
+  animations: [
+    trigger(
+      'fadeAnim', [
+        transition(':enter', [
+          style({transform: 'translateX(100%)'}),
+          animate('200ms', style({transform: 'translateX(0)'}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)'}),
+          animate('200ms', style({transform: 'translateX(100%)'}))
+        ])
+      ]
+    )
+  ]
 })
 export class PokeDetailsSidebarComponent implements OnInit {
 
-  constructor() { }
+  public showDetails: boolean;
+
+  constructor(
+    private store: Store<AppState>
+  ) {
+    this.store.select(state => state.pokemones).subscribe((pokemones) => {
+      if (pokemones && pokemones.list && pokemones.list.length > 0) {
+        if (pokemones.list.find((pokemon: Pokemon) => pokemon.selected)) {
+          this.showDetails = true;
+        } else {
+          this.showDetails = false;
+        }
+      }
+   });
+  }
 
   ngOnInit(): void {
   }
-
 }
